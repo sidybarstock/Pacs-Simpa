e901987cc74d41f0093e44b0cf06aef3cfa717f4
sidybarstock <sidy@barstock.ai>
2025-08-20 00:56:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -352,7 +352,11 @@ app.post('/admin/volunteers/add', requireAdmin, (req, res) => {
 
 // TODO: endpoints for creating/editing/deleting events, volunteers, products could be added here
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Start server when not on Vercel
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
